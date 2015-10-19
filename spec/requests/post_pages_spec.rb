aa663a430fcb3b047097f2e41eff07cafbefecc0
Nate Geslin <ngeslin@p1u7o13.local>
2015-10-18 22:28:55 -0500

    Adds posts controller * Adds posts controller tests * Adds stub for post_pages_spec test

---
+++ spec/requests/post_pages_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe "Post Pages" do
+    describe "index" do
+    end
+
+    describe "show" do
+    end
+
+    describe "new" do
+    end
+
+    describe "create" do
+    end
+
+    describe "edit" do
+    end
+
+    describe "destroy" do
+    end
+end
