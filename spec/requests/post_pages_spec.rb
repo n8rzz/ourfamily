cf8d14053cf2843615de1e4f03fc22f0c83877dc
Nate Geslin <teamtomkins23@gmail.com>
2015-10-19 21:24:09 -0500

    Adds application template tests for basic html/css structure * Adds post#index and posts partial * Adds post#index controller method

--- spec/requests/post_pages_spec.rb
+++ spec/requests/post_pages_spec.rb
@@ -5,14 +5,20 @@ describe "Post Pages" do
     subject { page }
 
     describe "index" do
+        let(:post) { FactoryGirl.create(:post) }
+
         before do
             visit root_path
         end
 
         it { should have_title('OurFamily') }
-        # it { should have_content('Recent Posts') }
-        it { should have_selector('h2', 'Recent Posts') }
+        it { should have_selector('h1', 'Recent Posts') }
         it { should have_selector('ul.vlist') }
+        it { should have_selector('li div.excerpt') }
+        it { should have_selector('div.excerpt-hd h2.hdg', post.title) }
+        it { should have_selector('div.excerpt-bd') }
+        it { should have_selector('div.excerpt-ft') }
+
     end
 
     describe "show" do
