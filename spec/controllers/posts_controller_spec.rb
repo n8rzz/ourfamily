a576d39c775e601f82096902bc4521364fb33d21
Nate Geslin <teamtomkins23@gmail.com>
2015-10-20 02:08:18 -0500

    Adds post edit template

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -11,11 +11,6 @@ RSpec.describe PostsController, type: :controller do
   end
 
   describe "#show" do
-    # before do
-    #   visit post_path
-    # end
-
-    # it { should render_template("show") }
   end
 
   describe "#new" do
@@ -23,47 +18,7 @@ RSpec.describe PostsController, type: :controller do
       visit new_post_path
     end
 
-    # it "returns http success" do
-    #   get :new
-    #   expect(response).to have_http_status(:success)
-    # end
-
       it { should render_template("new") }
   end
 
-  # describe "GET #create" do
-  #   it "returns http success" do
-  #     get :create
-  #     expect(response).to have_http_status(:success)
-  #   end
-
-  #   it "renders the create template" do
-  #     get :create
-  #     expect(response).to render_template("create")
-  #   end
-  # end
-
-  # describe "GET #update" do
-  #   it "returns http success" do
-  #     get :update
-  #     expect(response).to have_http_status(:success)
-  #   end
-
-  #   it "renders the update template" do
-  #     get :update
-  #     expect(response).to render_template("update")
-  #   end
-  # end
-
-  # describe "GET #destroy" do
-  #   it "returns http success" do
-  #     get :destroy
-  #     expect(response).to have_http_status(:success)
-  #   end
-
-  #   it "renders the show template" do
-  #     get :destroy
-  #     expect(response).to render_template("destroy")
-  #   end
-  # end
 end
--- spec/requests/post_pages_spec.rb
+++ spec/requests/post_pages_spec.rb
@@ -12,13 +12,6 @@ describe "Post Pages" do
 
         it { should have_title('OurFamily') }
         it { should have_selector('h1', 'Recent Posts') }
-        # it { should have_selector('div.excerpt') }
-        # it { should have_selector('li', text: post.title) }
-        # it { should have_selector('div.excerpt-hd') }
-        # it { should have_selector('h2.hdg', :text => post.title) }
-        # it { should have_selector('div.excerpt-bd', :text => post.body) }
-        # it { should have_selector('div.excerpt-ft') }
-
     end
 
     describe "show" do
@@ -30,9 +23,6 @@ describe "Post Pages" do
     end
 
     describe "new" do
-    end
-
-    describe "create" do
     end
 
     describe "edit" do
