9906075da1c07f558f1b9c8c9dc0e8753c456330
Nate Geslin <teamtomkins23@gmail.com>
2015-10-22 21:41:57 -0500

    Adds test for post.show controller

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -2,20 +2,33 @@ require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
   describe '#index' do
-    before do
-      visit posts_path
-    end
+    before { visit posts_path }
 
     it { should render_template('index') }
   end
 
   describe '#show' do
+    let(:post) { create(:post) }
+    before { visit post_path(post) }
+
+    it { should render_template('show') }
+
+    it 'should assign post to @post' do
+      get :show, id: post.id, title: post.title, body: post.body
+      expect(assigns(:post)).to eq post
+    end
+
+    context 'if it has a photo attachment' do
+      # let(:post) { create(:post_attachment) }
+
+      # it 'should exst' do
+      #   expect(post.photo.exists?).to eq(true)
+      # end
+    end
   end
 
   describe '#new' do
-    before do
-      visit new_post_path
-    end
+    before { visit new_post_path }
 
     it { should render_template('new') }
   end
