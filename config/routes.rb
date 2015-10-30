e2ba39763c95b93dba31166bac62926315d49eca
Nate Geslin <teamtomkins23@gmail.com>
2015-10-29 23:46:23 -0500

    Adds homescreen controller * Moves root route to homescreen#index * Adds homescreen index view * Adds simple tests for homescreen

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  root 'homescreen#index'
+
   devise_for :users,
              controllers: { registrations: 'registrations' },
              path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
-  root 'posts#index'
 
   resources :posts
 end
