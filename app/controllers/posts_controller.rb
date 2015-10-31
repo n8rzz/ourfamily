47f96dbb16032acfa0cd2ccb7ee4d8c54aa85411
Nate Geslin <teamtomkins23@gmail.com>
2015-10-31 01:04:45 -0500

    Removes posts#index from from routes, controller and view * Updates tests

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,6 @@
 class PostsController < ApplicationController
   before_action :correct_user, only: :destroy
 
-  def index
-    @posts = Post.all
-  end
-
   def show
     @post = Post.find(params[:id])
     @newer = @post.newer
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
              controllers: { registrations: 'registrations' },
              path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
 
-  resources :posts
+  # TODO: index route needs to be removed completely once static_pages controller is built
+  resources :posts, except: :index
 end
