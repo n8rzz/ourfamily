f7ec4a89ea24b0e1415c0570aa5b8f2eea9fdbef
Nate Geslin <teamtomkins23@gmail.com>
2015-11-23 23:19:07 -0600

    Moves wrapper inside grid * Updates views with wrapper * adds js-alertView to alert partial * Adds style changes to hlist, vlist, and exceprt

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,10 +2,26 @@ import $ from 'jquery-browserify';
 // import Drawer from './controllers/Drawer';
 // import Gallery from './controllers/Gallery';
 
+// export default class App {
+//   constructor() {
+//       this.$element = $('[data-controller="js-app"]');
+//
+//       return this._init();
+//   }
+//
+//   _init() {
+//
+//   }
+// }
+
+
+
 var App = function() {
   var $element = $('[data-controller="js-app"]');
 
-  console.log('app', this);
+  console.log('app', $element);
+
+  var $alertView = $element.find('[data-controller="js-alertView"]');
 
   var $drawer = $element.find('[data-controller="js-drawer"]');
   var $drawerTrigger = $element.find('[data-controller="js-drawerTrigger"]');
