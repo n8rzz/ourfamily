8655db7c48970fe4e435e50e21a6bad0ff95707d
Nate Geslin <teamtomkins23@gmail.com>
2015-11-29 00:36:28 -0600

    Adds AlertView class * Adds transition delay and speed * Adds checks for null element and quick exit if undefined

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,62 +1,86 @@
 import $ from 'jquery-browserify';
 // import Base from './util/Base';
 import DrawerView from './Drawer/DrawerView';
+import AlertView from './Alert/AlertView';
 // import Gallery from './controllers/Gallery';
 
+/**
+ * Base view controller
+ *  All other views and controllers are instantiated from this class
+ *
+ * @class App
+ * @author Nate Geslin
+ */
 export default class App {
     constructor($element) {
         this.$element = $element;
-        this.$drawer = null;
+        this.$drawer = null
+        this.drawrerView = null;
+        this.$alert = null;
+        this.alertView = null;
 
         return this._init();
     }
 
+    /**
+     * @method _init
+     * @private
+     * @chainable
+     */
     _init() {
 
         return this._createChildren()
                    ._enable();
     }
 
+    /**
+     * @method _createChildren
+     * @private
+     * @chainable
+     */
     _createChildren() {
-        console.log('app');
         this.$drawer = this.$element.find('[data-controller="js-drawer"]');
+        this.$alert = this.$element.find('[data-controller="js-alertView"]');
+
+        console.log('app');
 
         return this;
     }
 
+    /**
+     * @method _enable
+     * @private
+     * @chainable
+     */
     _enable() {
         this.drawerView = new DrawerView(this.$drawer);
+        this.alertView = new AlertView(this.$alert);
 
         return this;
     }
 
+    /**
+     * @method _disable
+     * @private
+     * @chainable
+     */
     _disable() {
+
+        return this._destroy();
+    }
+
+    /**
+     * @method _destroy
+     * @private
+     * @chainable
+     */
+    _destroy() {
+        this.$element = null;
         this.drawerView = null;
         this.$drawer = null;
+        this.alertView = null;
+        this.$alert = null;
 
         return this;
     }
 }
-
-
-
-// var App = function() {
-//   var $element = $('[data-controller="js-app"]');
-//
-//   console.log('app', $element);
-//
-//   var $alertView = $element.find('[data-controller="js-alertView"]');
-//
-//   var $drawer = $element.find('[data-controller="js-drawer"]');
-//   var $drawerTrigger = $element.find('[data-controller="js-drawerTrigger"]');
-//   var $drawerView = $element.find('[data-controller="js-drawerView"]');
-//
-//   $drawerTrigger.on('click', function(e) {
-//     e.preventDefault();
-//
-//     $drawerView.slideToggle();
-//   });
-//
-// }
-//
-// var app = App();
