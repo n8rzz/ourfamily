2f183636b0ad41104d7aa742c7045a7d08c12675
nate geslin <teamtomkins23@gmail.com>
2021-04-01 08:35:19 -0500

    removes registration route

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   get 'home' => 'homescreen#index'
 
   devise_for :users,
-             controllers: { registrations: 'registrations' },
-             path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+            #  controllers: { registrations: 'registrations' },
+             path_names: { sign_in: 'login', sign_out: 'logout'} #, sign_up: 'register' }
 
   resources :posts, except: :index
   resources :images, except: [:index, :edit, :update]
